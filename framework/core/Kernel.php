9ed2d026200227617d1c9f21651901f40ace6f8f
JulianH99 <juliancorredors99@outlook.com>
2018-03-15 15:31:42 -0500

    Added MethodNotFoundExcetion. Added a VIEWS_DIR constant and changed the way of cheking the existence of the controller in controllersdef.php

--- framework/core/Kernel.php
+++ framework/core/Kernel.php
@@ -4,6 +4,7 @@ namespace Framework\Core;
 
 
 use Framework\Lib\Exceptions\ControllerNotFoundException;
+use Framework\Lib\Exceptions\MethodNotFoundException;
 /**
  * 
  */
@@ -29,6 +30,8 @@ class Kernel {
 
         define("CONTROLLERS_DIR", APP_DIR . 'controllers' . DS);
 
+        define("VIEWS_DIR", ROOT_DIR . 'views' . DS);
+
         //define("MODELS_DIR", APP_DIR  'models' . DS);
 
         //define("VIEWS_DIR", ROOT_DIR . 'views' . DS);
@@ -48,19 +51,19 @@ class Kernel {
         
         $controllers  = require ROOT_DIR . 'controllersdef.php';
 
-        if(in_array(CONTROLLER, $controllers)) {
-
-            $controller = new $controller[CONTROLLER];
+        if(isset($controllers[CURRENT_CONTROLLER])) {
+            
+            $controller = new $controllers[CURRENT_CONTROLLER]();
             if(method_exists($controller, CURRENT_METHOD)) {
                 $method = CURRENT_METHOD;
-
+               
                 $controller->$method();
             }else {
-                throw new MethodNotDefinedException($method);
+                throw new MethodNotFoundException(CURRENT_METHOD, $controller);
             }
 
         }else {
-            throw new ControllerNotFoundException(CONTROLLER);
+            throw new ControllerNotFoundException(CURRENT_CONTROLLER);
         }
 
 
