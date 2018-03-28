bcf8e2d83e3a5d4c1e0c389fbb814905e3eb0a98
JulianH99 <juliancorredors99@outlook.com>
2018-03-28 18:44:48 -0500

    First version of the 'apply' method in Effect.php

--- src/models/accessories/Effect.php
+++ src/models/accessories/Effect.php
@@ -6,8 +6,12 @@ namespace App\Models\Accessories;
 use App\Interfaces\Applicable;
 
 /**
- * Class Effect
+ * Class Effect. Represents the effect that can be applied when a user picks a
+ * specific weapon
+ * @author Julian Barrios
  * @package App\Models\Accessories
+ * @version 0.1
+ *
  */
 class Effect implements Applicable{
     private $affected_attribute = '';
@@ -23,6 +27,9 @@ class Effect implements Applicable{
     }
 
     /**
+     * Applies the effect to the $object, checking if this
+     * has the attribute defined in $this->affected attribute.
+     * @see \App\Interfaces\Applicable
      * @param $object
      * @return bool
      */
@@ -30,7 +37,21 @@ class Effect implements Applicable{
 
         if (property_exists($object, $this->affected_attribute)) {
 
+            $setter_method = "set" . ucfirst($this->affected_attribute);
+            $getter_method = "get" . ucfirst($this->affected_attribute);
+
+            if (method_exists($object, $getter_method)
+                    && method_exists($object, $setter_method)) {
+                $current_value = $object->$getter_method();
+                $new_value = $current_value + $this->value;
+                $object->$setter_method($new_value);
+
+                return true;
+
+
+            }
         }
+        return false;
     }
 
 
